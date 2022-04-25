1784c664ea1588071d6cae86c5a6bdd19d0f0226
Gabriel Augendre <gabriel@augendre.info>
2022-04-25 16:27:59 +0200

    Add delete and list views for baskets

--- src/purchase/urls.py
+++ src/purchase/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from purchase.views import NewBasketView, UpdateBasketView
+from purchase.views import (
+    DeleteBasketView,
+    ListBasketsView,
+    NewBasketView,
+    UpdateBasketView,
+)
 
 app_name = "purchase"
 urlpatterns = [
+    path("", ListBasketsView.as_view(), name="list"),
     path("new/", NewBasketView.as_view(), name="new"),
-    path("update/<int:pk>/", UpdateBasketView.as_view(), name="update"),
+    path("<int:pk>/update/", UpdateBasketView.as_view(), name="update"),
+    path("<int:pk>/delete/", DeleteBasketView.as_view(), name="delete"),
 ]
--- src/purchase/views.py
+++ src/purchase/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from purchase.forms import BasketForm
 from purchase.models import Basket
@@ -20,3 +20,18 @@ class UpdateBasketView(ProtectedViewsMixin, UpdateView):
     permission_required = ["purchase.change_basket", "purchase.view_basket"]
     model = Basket
     form_class = BasketForm
+
+
+class ListBasketsView(ProtectedViewsMixin, ListView):
+    permission_required = ["purchase.view_basket"]
+    model = Basket
+    context_object_name = "baskets"
+    ordering = "-id"
+
+
+class DeleteBasketView(ProtectedViewsMixin, DeleteView):
+    permission_required = ["purchase.delete_basket"]
+    model = Basket
+
+    def get_success_url(self):
+        return reverse("purchase:list")
