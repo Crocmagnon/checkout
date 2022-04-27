282318ac81449c181c798ddb5994007c9278c162
Gabriel Augendre <gabriel@augendre.info>
2022-04-27 22:59:52 +0200

    Moar graphs

--- src/purchase/views/reports.py
+++ src/purchase/views/reports.py
@@ -1,11 +1,18 @@
 import datetime
 from io import StringIO
+from zoneinfo import ZoneInfo
 
+import numpy as np
+from django.conf import settings
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
 from matplotlib import pyplot as plt
 from matplotlib import ticker
+from matplotlib.axes import Axes
+from matplotlib.container import BarContainer
+from matplotlib.dates import AutoDateLocator, ConciseDateFormatter, HourLocator
+from matplotlib.figure import Figure
 
 from purchase.models import Basket, PaymentMethod, Product, ProductQuerySet
 from purchase.views.utils import ProtectedViewsMixin
@@ -31,7 +38,11 @@ class ReportsView(ProtectedViewsMixin, TemplateView):
         }
 
         products = Product.objects.with_turnover().with_sold()
-        products_plot = self.get_products_plot(products)
+        (
+            products_plot,
+            products_sold_pie,
+            products_turnover_pie,
+        ) = self.get_products_plots(products)
         by_hour_plot = self.by_hour_plot(baskets)
         context.update(
             {
@@ -41,6 +52,8 @@ class ReportsView(ProtectedViewsMixin, TemplateView):
                 "average_basket_by_day": average_basket_by_day,
                 "products": products,
                 "products_plot": products_plot,
+                "products_sold_pie": products_sold_pie,
+                "products_turnover_pie": products_turnover_pie,
                 "by_hour_plot": by_hour_plot,
                 "payment_methods": PaymentMethod.objects.with_turnover().with_sold(),
                 "no_payment_method": Basket.objects.no_payment_method().priced(),
@@ -48,26 +61,49 @@ class ReportsView(ProtectedViewsMixin, TemplateView):
         )
         return context
 
-    def get_products_plot(self, products: ProductQuerySet):
+    def get_products_plots(self, products: ProductQuerySet):
         labels, sold, turnover = self.get_products_data_for_plot(products)
 
-        fig, ax1 = plt.subplots()
+        x = np.arange(len(labels))
+        width = 0.4
+        fig: Figure = plt.figure()
+        fig.suptitle(_("Sales by product"))
+
         color = "tab:orange"
-        ax1.bar(labels, sold, width=0.8, color=color)
+        ax1: Axes = fig.add_subplot()
+        bar: BarContainer = ax1.bar(x - width / 2, sold, width=width, color=color)
+        ax1.bar_label(bar)
         ax1.tick_params(axis="x", rotation=15)
+        ax1.tick_params(axis="y", labelcolor=color)
+        ax1.set_xticks(x, labels)
         ax1.set_ylabel(_("# sold"), color=color)
 
         color = "tab:blue"
-        ax2 = ax1.twinx()
-        ax2.bar(labels, turnover, width=0.4, color=color)
+        ax2: Axes = ax1.twinx()
+        bar = ax2.bar(x + width / 2, turnover, width=width, color=color)
+        ax2.bar_label(bar, fmt="%d€")
         ax2.set_ylabel(_("Turnover by product"), color=color)
-        plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter("%.2f€"))
+        ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter("%.2f€"))
+        ax2.tick_params(axis="y", labelcolor=color)
 
         fig.tight_layout()
-        image_data = StringIO()
-        fig.savefig(image_data, format="svg")
-        image_data.seek(0)
-        return image_data.getvalue()
+        img1 = self.get_image_from_fig(fig)
+
+        fig = plt.figure()
+        fig.suptitle(_("# sold"))
+        ax1 = fig.add_subplot()
+        ax1.pie(sold, labels=labels, autopct="%d%%")
+        fig.tight_layout()
+        img2 = self.get_image_from_fig(fig)
+
+        fig = plt.figure()
+        fig.suptitle(_("Turnover by product"))
+        ax1 = fig.add_subplot()
+        ax1.pie(turnover, labels=labels, autopct="%d%%")
+        fig.tight_layout()
+        img3 = self.get_image_from_fig(fig)
+
+        return img1, img2, img3
 
     def get_products_data_for_plot(self, products):
         labels = []
@@ -80,25 +116,36 @@ class ReportsView(ProtectedViewsMixin, TemplateView):
         return labels, sold, turnover
 
     def by_hour_plot(self, baskets):
-        counts, labels, turnovers = self.get_by_hour_data_for_plot(baskets)
-        fig, ax1 = plt.subplots()
+        labels, counts, turnovers = self.get_by_hour_data_for_plot(baskets)
         hours_in_day = 24
+        fig: Figure = plt.figure()
+        fig.suptitle(_("Sales by hour"))
+        ax1: Axes = fig.add_subplot()
+
         color = "tab:orange"
         ax1.bar(labels, counts, width=1 / hours_in_day, color=color)
         ax1.tick_params(axis="x", rotation=15)
+        ax1.xaxis.set_minor_locator(HourLocator())
+        tz = ZoneInfo(settings.TIME_ZONE)
+        locator = AutoDateLocator(tz=tz)
+        ax1.xaxis.set_major_locator(locator)
+        ax1.xaxis.set_major_formatter(ConciseDateFormatter(locator, tz=tz))
         ax1.set_ylabel(_("Basket count by hour"), color=color)
+        ax1.set_yticks(np.linspace(*ax1.get_ybound(), 8))
+        ax1.tick_params(axis="y", labelcolor=color)
+        ax1.grid(visible=True, which="major", axis="y")
 
         color = "tab:blue"
-        ax2 = ax1.twinx()
-        ax2.bar(labels, turnovers, width=1 / (hours_in_day * 2), color=color)
+        ax2: Axes = ax1.twinx()
+        ax2.plot(labels, turnovers, ".-", color=color)
         ax2.set_ylabel(_("Turnover by hour"), color=color)
-        plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter("%.2f€"))
+        ax2.set_ylim(bottom=0)
+        ax2.set_yticks(np.linspace(*ax2.get_ybound(), 8))
+        ax2.tick_params(axis="y", labelcolor=color)
+        ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter("%.2f€"))
 
         fig.tight_layout()
-        image_data = StringIO()
-        fig.savefig(image_data, format="svg")
-        image_data.seek(0)
-        return image_data.getvalue()
+        return self.get_image_from_fig(fig)
 
     def get_by_hour_data_for_plot(self, baskets):
         current: datetime.datetime = baskets[0].created_at
@@ -120,8 +167,15 @@ class ReportsView(ProtectedViewsMixin, TemplateView):
                 if basket_index == len(baskets):
                     break
                 basket = baskets[basket_index]
-            labels.append(current)
+            labels.append(current.astimezone(ZoneInfo(settings.TIME_ZONE)))
             counts.append(count)
             turnovers.append(turnover)
             current = end_slot
-        return counts, labels, turnovers
+        return labels, counts, turnovers
+
+    def get_image_from_fig(self, fig):
+        image_data = StringIO()
+        fig.savefig(image_data, format="svg")
+        image_data.seek(0)
+        img1 = image_data.getvalue()
+        return img1
