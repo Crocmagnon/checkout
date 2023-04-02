84d003e2e0864bd0e24b29eb011720fb9f2f93e0
Gabriel Augendre <gabriel@augendre.info>
2023-04-02 18:33:43 +0200

    Add refresh cache command

---
+++ src/purchase/management/commands/refresh_cache.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from purchase.models import Cache
+
+
+class Command(BaseCommand):
+    help = "Refresh cache"  # noqa: A003
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        Cache.get_solo().refresh()
